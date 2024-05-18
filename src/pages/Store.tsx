248c350d7cb69bf0d4969155fcd81024c89752a5
Eliz Erdim <elizerdim@gmail.com>
2024-05-18 17:11:33 +0300

    Add Store page content

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,5 +1,18 @@
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import StoreItem from "../components/StoreItem";
+
 export default function Store() {
   return (
-    <div>Store</div>
-  )
+    <>
+      <h1>Store</h1>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 }
